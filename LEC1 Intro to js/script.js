269c471c494d05abb6a0412c07938b28c9bf5c4f
Khurram Bashir Raja <khurrambashirraja@gmail.com>
2026-02-04 22:29:37 +0500

    Intro to JS

---
+++ LEC1 Intro to js/script.js
@@ -0,0 +1,9 @@
+// alert("Hello World!");
+
+console.log("code is running...");
+console.log("code is running very well...");
+console.log("code is running like a wow...");
+
+
+// var a = prompt("ENTER YOUR NUMBER...")
+// console.log("YOUR NUMBER IS " + a )
